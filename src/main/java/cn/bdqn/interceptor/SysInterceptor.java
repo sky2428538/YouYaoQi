d56b21ce2dac212590f6b515a5f7b1c8c9bb0c39
fengjun0328 <783585655@qq.com>
2018-12-23 14:36:02 +0800

    Initial commit

---
+++ src/main/java/cn/bdqn/interceptor/SysInterceptor.java
@@ -0,0 +1,15 @@
+package cn.bdqn.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class SysInterceptor extends HandlerInterceptorAdapter {
+	private Logger logger = Logger.getLogger(SysInterceptor.class);
+	
+	public boolean preHandle(HttpServletRequest request,HttpServletResponse response,Object handler) throws Exception{
+		logger.debug("SysInterceptor preHandle ==========================");
+		return true;
+	}
+}
